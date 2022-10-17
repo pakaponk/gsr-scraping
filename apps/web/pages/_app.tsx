6dfd666edb0c25bc333d91bd77951b6f55a0ab7f
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-17 19:30:07 +0700

    :wrench: Setup React Query

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: Record<string, unknown> }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
