af32223ff8ecdd6fd22ea27ccf7697587d7167e0
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-17 16:15:42 +0700

    :fire: Remove unnecessary semicolon

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
