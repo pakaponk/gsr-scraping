122b642ac00e5337059c2ef07ff4be9df34f07ed
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-17 23:02:07 +0700

    :sparkles: Validate Login Form before submit

--- apps/web/src/Components/LoginForm.tsx
+++ apps/web/src/Components/LoginForm.tsx
@@ -1,15 +1,53 @@
-import { Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+
+type FormValue = {
+  email: string;
+  password: string;
+};
 
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>();
+
+  const onValid: Parameters<typeof handleSubmit>[0] = ({ email, password }) => {
+    // TODO: Send Login Request
+  };
+
   return (
-    <Stack as="form" spacing={4} width="full">
-      <FormControl>
+    <Stack as="form" spacing={4} width="full" onSubmit={handleSubmit(onValid)}>
+      <FormControl isInvalid={!!errors.email}>
         <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input type="email" name="email" placeholder="Email" />
+        <Input
+          {...register('email', {
+            required: 'Please fill your email',
+            setValueAs: (value) => value.trim(),
+          })}
+          type="email"
+          placeholder="Email"
+        />
+        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={!!errors.password}>
         <FormLabel htmlFor="password">Password</FormLabel>
-        <Input type="password" name="password" placeholder="Password" />
+        <Input
+          {...register('password', {
+            required: 'Please fill your password',
+          })}
+          type="password"
+          placeholder="Password"
+        />
+        <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
       </FormControl>
       <Button type="submit" variant="solid" colorScheme="blue">
         Sign in
