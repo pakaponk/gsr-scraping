3c288dbc2aa12b06cbb0b2f66a79d937abf04910
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-17 15:42:19 +0700

    :wrench: Setup Chakra UI

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,14 @@
+import { Center, Container, Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello Next.js</div>;
+  return (
+    <Container display="flex" minH="100vh">
+      <Center flexGrow="1">
+        <Heading>Hello Next.js</Heading>
+      </Center>
+    </Container>
+  );
 };
 
 export default Home;
