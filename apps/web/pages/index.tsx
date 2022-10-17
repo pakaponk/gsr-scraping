8acb97ce89f96cc0dea893e76b7009395b969250
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-17 16:41:42 +0700

    :sparkles: Add Login Form

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,66 @@
-import { Center, Container, Heading } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
+
+function LoginForm() {
+  return (
+    <Stack as="form" spacing={4} width="full">
+      <FormControl>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input type="email" name="email" placeholder="Email" />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input type="password" name="password" placeholder="Password" />
+      </FormControl>
+      <Button type="submit" variant="solid" colorScheme="blue">
+        Login
+      </Button>
+    </Stack>
+  );
+}
 
 const Home: NextPage = () => {
   return (
-    <Container display="flex" minH="100vh">
-      <Center flexGrow="1">
-        <Heading>Hello Next.js</Heading>
-      </Center>
-    </Container>
+    <Flex direction="column" bgColor="gray.100">
+      <Container display="flex" minH="100vh">
+        <VStack
+          spacing="8"
+          justifyContent="center"
+          alignItems="center"
+          flexGrow="1"
+        >
+          <VStack spacing="2">
+            <Heading textAlign="center">Sign in to your account</Heading>
+            <Text>
+              or{' '}
+              <Button variant="link" colorScheme="blue">
+                create a new account
+              </Button>
+            </Text>
+          </VStack>
+          <Flex
+            minWidth={{ base: 'calc(100vw - 32px)', sm: '400px' }}
+            p={8}
+            bgColor="white"
+            shadow="md"
+            borderRadius="8px"
+          >
+            <LoginForm />
+          </Flex>
+        </VStack>
+      </Container>
+    </Flex>
   );
 };
 
