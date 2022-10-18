1f75e27148b7b738ca914e3e40cbaa4fae0aa67d
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-18 23:27:54 +0700

    :recycle: Use builder in seed script

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,20 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 import * as argon2 from 'argon2';
+import { userBuilder, reportBuilder } from '../test/utils/mock';
 
 const prisma = new PrismaClient();
 
 const mockUsersPromise = Promise.all(
   [
-    {
-      name: 'John Doe',
-      email: 'john.doe@pakapon.dev',
-      password: '1q2w3e4r',
-    },
-    {
-      name: 'Jane Doe',
-      email: 'jane.doe@pakapon.dev',
-      password: '1q2w3e4r',
-    },
+    userBuilder({
+      overrides: {
+        name: 'John Doe',
+        email: 'john.doe@pakaponk.dev',
+        password: '1q2w3e4r',
+      },
+    }),
+    userBuilder({
+      overrides: {
+        name: 'Jane Doe',
+        email: 'jane.doe@pakaponk.dev',
+        password: '1q2w3e4r',
+      },
+    }),
   ].map(async (user) => {
     const hashedPassword = await argon2.hash(user.password);
     return {
