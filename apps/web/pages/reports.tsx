7824d3ec5124f9093afcc98321333583d8d074a0
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-19 00:35:59 +0700

    :sparkles: Display Loading while retrieving the current user

--- apps/web/pages/reports.tsx
+++ apps/web/pages/reports.tsx
@@ -1,16 +1,37 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Center, Container, Heading, Spinner } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { Navbar } from '../src/Components/Navbar';
+import { useAuth } from '../src/Hooks/useAuth';
 
 const Reports: NextPage = () => {
-  return (
-    <Box pt="64px" bgColor="gray.100">
-      <Navbar />
-      <Container py={12} minHeight="calc(100vh - 64px)" maxWidth="container.xl">
-        <Heading>Reports</Heading>
-      </Container>
-    </Box>
-  );
+  const [{ authState }] = useAuth();
+
+  switch (authState) {
+    case 'PENDING': {
+      return (
+        <Center bgColor="gray.100" minHeight="100vh">
+          <Spinner size="xl" color="blue.500" />
+        </Center>
+      );
+    }
+    case 'AUTHENTICATED': {
+      return (
+        <Box pt="64px" bgColor="gray.100">
+          <Navbar />
+          <Container
+            py={12}
+            minHeight="calc(100vh - 64px)"
+            maxWidth="container.xl"
+          >
+            <Heading>Reports</Heading>
+          </Container>
+        </Box>
+      );
+    }
+    default: {
+      return null;
+    }
+  }
 };
 
 export default Reports;
